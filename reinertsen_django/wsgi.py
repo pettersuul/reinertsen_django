396a5b492c91ffbdbc7537ef31bc71c1ab1de879
Petter Suul <petter.suul@blank.no>
2018-04-13 13:46:25 +0200

    edited settings

--- reinertsen_django/wsgi.py
+++ reinertsen_django/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_contentful_demo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reinertsen_django.settings")
 
 application = get_wsgi_application()
