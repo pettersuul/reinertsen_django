7db4614f7af3093ff26e760ddc667d3247d301ad
Petter Suul <petter.suul@blank.no>
2018-06-04 13:27:40 +0200

    Tried reverting to original static settings

--- reinertsen_django/settings.py
+++ reinertsen_django/settings.py
@@ -105,21 +105,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
