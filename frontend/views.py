78a46d41085d067c60fb36c363a7c9ac7666c613
Petter Suul <petter.suul@blank.no>
2018-06-25 16:20:34 +0200

    Added locale to views

--- frontend/views.py
+++ frontend/views.py
@@ -2,61 +2,70 @@ from django.http import Http404, HttpRequest
 from django.shortcuts import render
 from .models import client
 
+def checklocale(domain):
+    eng = '.com'
+    no  = '.no'
+
+    if domain.endswith(no):
+        return 'nb-NO'
+    else:
+        return 'en-US'
+
 # Create your views here.
 
 def index(request):
+    locale = checklocale(request.get_host()),
     return render(request, 'index.html', {
-        'domain': request.get_host(),
-        'pages': client.entries({'content_type': 'page', 'order': 'fields.order', 'limit': 3}),
-        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-        'listings': client.entries({'content_type': 'page', 'fields.slug': 'work-listings'})[0],
-        'contact':  client.entries({'content_type': 'contact'})[0],
-        'footer':  client.entries({'content_type': 'footer'})[0]
+        'pages': client.entries({'content_type': 'page', 'order': 'fields.order', 'limit': 3, 'locale': locale}),
+        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+        'listings': client.entries({'content_type': 'page', 'fields.slug': 'work-listings', 'locale': locale})[0],
+        'contact':  client.entries({'content_type': 'contact', 'locale': locale})[0],
+        'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
     })
 
 def references(request):
     return render(request, 'references.html', {
-        'page': client.entries({'content_type': 'page', 'fields.slug': 'references'})[0],
-        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-        'footer':  client.entries({'content_type': 'footer'})[0]
+        'page': client.entries({'content_type': 'page', 'fields.slug': 'references', 'locale': locale})[0],
+        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+        'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
         })
 
 def reference_single(request, slug):
     try:
-        reference = client.entries({'content_type': 'reference', 'fields.slug': slug})[0]
+        reference = client.entries({'content_type': 'reference', 'fields.slug': slug, 'locale': locale})[0]
         return render(request, 'reference_single.html', {
             'reference': reference,
-            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-            'footer':  client.entries({'content_type': 'footer'})[0]
+            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+            'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
             })
     except IndexError:
         raise Http404('Post not found for slug: {0}'.format(slug))
 
 def listings(request):
     return render(request, 'work.html', {
-        'page': client.entries({'content_type': 'page', 'fields.slug': 'work-listings'})[0],
-        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-        'footer':  client.entries({'content_type': 'footer'})[0]
+        'page': client.entries({'content_type': 'page', 'fields.slug': 'work-listings', 'locale': locale})[0],
+        'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+        'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
         })
 
 def listings_single(request, slug):
     try:
-        listing = client.entries({'content_type': 'listing', 'fields.slug': slug})[0]
+        listing = client.entries({'content_type': 'listing', 'fields.slug': slug, 'locale': locale})[0]
         return render(request, 'work-single.html', {
             'listing': listing,
-            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-            'footer':  client.entries({'content_type': 'footer'})[0]
+            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+            'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
             })
     except IndexError:
         raise Http404('Post not found for slug: {0}'.format(slug))
 
 def page(request, slug):
     try:
-        page = client.entries({'content_type': 'page', 'fields.slug': slug})[0]
+        page = client.entries({'content_type': 'page', 'fields.slug': slug, 'locale': locale})[0]
         return render(request, 'page.html', {
             'page': page,
-            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order'}),
-            'footer':  client.entries({'content_type': 'footer'})[0]
+            'nav':  client.entries({'content_type': 'page', 'order': 'fields.order', 'locale': locale}),
+            'footer':  client.entries({'content_type': 'footer', 'locale': locale})[0]
             })
     except IndexError:
         raise Http404('Post not found for slug: {0}'.format(slug))
